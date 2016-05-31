881108740fa7638deeabf874a214220df8ec9d13
Ashwin Sinha <ash@ds.gg>
2016-05-30 21:16:41 -0700

    Can write to ProgramData as admin now.
    updated app manifest to require admin permission.

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using IWshRuntimeLibrary; //For creating shortcuts (Add References > COM > Windows Script Host Object)
 
 namespace Windows_Add_To_Start
 {
@@ -28,7 +31,56 @@ namespace Windows_Add_To_Start
         {
             InitializeComponent();
             string[] args = Environment.GetCommandLineArgs();
-            MessageBox.Show(args[1]);
+            //MessageBox.Show(args[1]);
+            CopyToStartMenu(args[1]);
+            //close the form
+            Close();
         }
+
+        void CopyToStartMenu(string path)
+        {
+            //Get win root
+            string windowsDrive = Path.GetPathRoot(Environment.SystemDirectory);
+            //path to start menu
+            string startMenuPath = "ProgramData\\Microsoft\\Windows\\Start Menu\\Programs";
+            //Get filename from path
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            //Get extension
+            string extension = Path.GetExtension(path);
+
+            //If the path is not a shortcut
+            if (extension != ".lnk" || extension != ".url")
+            {
+                CreateShortcut(fileName, windowsDrive + startMenuPath, path);
+            }
+            else
+            {
+
+            }
+            //return;
+        }
+
+        public static void CreateShortcut(string shortcutName, string shortcutPath, string targetFileLocation)
+        {
+            string shortcutLocation = System.IO.Path.Combine(shortcutPath, shortcutName + ".lnk");
+            MessageBox.Show(shortcutLocation);
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutLocation);
+            shortcut.WorkingDirectory = Application.StartupPath; //?
+            //shortcut.IconLocation = @"c:\myicon.ico";           // The icon of the shortcut
+            shortcut.TargetPath = targetFileLocation;            // The path of the file that will launch when the shortcut is run
+
+            try
+            {
+                shortcut.Save();
+                MessageBox.Show("created");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+
     }
 }
