ade48782c5a53924dc016beb48718a372ac2f824
Ashwin Sinha <ash@ds.gg>
2016-05-30 20:30:44 -0700

    Getting context input

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,23 @@ namespace Windows_Add_To_Start
 {
     public partial class Form1 : Form
     {
+        /// <summary>
+        /// Setting default form visibility to hidden on load.
+        /// Form is only set to be visible when user input is required.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+            base.OnLoad(e);
+        }
+
         public Form1()
         {
             InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            MessageBox.Show(args[1]);
         }
     }
 }
